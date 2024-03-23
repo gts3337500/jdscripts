a3c7c82c2f269fc7426aa2b5d9b0e5c69af2df83
gts3337500 <80728681+gts3337500@users.noreply.github.com>
2024-03-24 00:03:02 +0800

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,7 @@
 [rewrite_local]
 
 # > 拦截100解锁超级会员
-^https:\/\/yunbusiness\.ccb\.com\/clp_service\/txCtrl\?txcode=[A-Za-z0-9]+$ url script-response-body https://github.com/gts3337500/jdscripts/blob/main/test.js
+^https:\/\/yunbusiness\.ccb\.com\/clp_service\/txCtrl\?txcode=[A-Za-z0-9]+$ url script-response-body https://raw.githubusercontent.com/gts3337500/jdscripts/main/test.js
 
 [mitm] 
 hostname = yunbusiness.ccb.com
