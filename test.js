50604a7c4f187b94cccdd5addda9804d18d44354
gts3337500 <80728681+gts3337500@users.noreply.github.com>
2024-03-24 00:01:25 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,13 @@
+
+
+[rewrite_local]
+
+# > 拦截100解锁超级会员
+^https:\/\/yunbusiness\.ccb\.com\/clp_service\/txCtrl\?txcode=[A-Za-z0-9]+$ url script-response-body https://github.com/gts3337500/jdscripts/blob/main/test.js
+
+[mitm] 
+hostname = yunbusiness.ccb.com
+
 var response = JSON.parse($response.body);
 
 // 替换VERNAME字段的值为"202403231001"
