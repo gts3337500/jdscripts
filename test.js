98529094b7759c24f8eed3ea4aa9a0663f2626ec
gts3337500 <80728681+gts3337500@users.noreply.github.com>
2024-03-23 23:46:03 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,14 @@
+$done({
+    body: JSON.stringify({
+        "errMsg": "",
+        "data": {
+            "INTERVAL": "20",
+            "VERNAME": "202403231001",
+            "VER": "43.75",
+            "LOAD": "0",
+            "URL": "https://res.yunbusiness.ccb.com/gbchannel/e_report/CCBLIFE.zip?v=4375",
+            "ONLINE": "0"
+        },
+        "errCode": "0"
+    })
+});
