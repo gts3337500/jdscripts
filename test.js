21814bfd5cbeaa2314b1ccece319a2ef6f652c6c
gts3337500 <80728681+gts3337500@users.noreply.github.com>
2024-03-26 01:40:48 +0800

    Update test.js

--- test.js
+++ test.js
@@ -19,7 +19,7 @@ if (isGetCookie = typeof $request !== `undefined`) {
 
 // 获取签到数据
 function GetCookie() {
-    $.log($response.body);
+    $.log($response);
     $.msg("开始替换变量")
     var response = JSON.parse(resp.body);
     response.data.MSPS_ENTITY[0].EFFECT_PERIOD_START = "20240326011000"
