e16f5672dd4e203db0cfa65e51854c6c41fd2678
gts3337500 <80728681+gts3337500@users.noreply.github.com>
2024-03-26 00:58:42 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,17 +1,6 @@
-
-
-[rewrite_local]
-
-# > 拦截100解锁超级会员
-^https:\/\/yunbusiness\.ccb\.com\/clp_service\/txCtrl\?txcode=[A-Za-z0-9]+$ url script-response-body https://raw.githubusercontent.com/gts3337500/jdscripts/main/test.js
-
-[mitm] 
-hostname = yunbusiness.ccb.com
-
-var response = JSON.parse($response.body);
-
-// 替换VERNAME字段的值为"202403231001"
-response.data.VERNAME = "202403231001";
+$.msg("开始替换变量")
+var response = JSON.parse(resp.body);
+response.data.MSPS_ENTITY[0].EFFECT_PERIOD_START = "20240326011000"
 
 $done({
   body: JSON.stringify(response)
